a6e19f60a6c4a0d2b164c7b7407e341b92c4e1f7
양윤재 <diddbswo04@dgsw.hs.kr>
2021-09-06 17:33:50 +0900

    [FIX] MainActivity Navigation Component과 BottomNavigationView 메뉴 연결

--- app/src/main/java/com/hansarang/android/air/ui/activity/MainActivity.kt
+++ app/src/main/java/com/hansarang/android/air/ui/activity/MainActivity.kt
@@ -3,11 +3,35 @@ package com.hansarang.android.air.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hansarang.android.air.R
+import com.hansarang.android.air.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        navController = findNavController(binding.navHostFragmentMain.id)
+        binding.bottomNavigationViewMain.setupWithNavController(navController)
+    }
+
 }
