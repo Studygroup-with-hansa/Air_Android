8258ee2bdc380068b764bcad070e312690eca0bf
양윤재 <diddbswo04@dgsw.hs.kr>
2021-09-07 11:51:44 +0900

    [ADD] Toolbar와 DrawerLayout을 NavController을 이용해 연결

--- app/src/main/java/com/hansarang/android/air/ui/activity/MainActivity.kt
+++ app/src/main/java/com/hansarang/android/air/ui/activity/MainActivity.kt
@@ -4,12 +4,16 @@ package com.hansarang.android.air.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.appcompat.widget.Toolbar
+import androidx.customview.widget.Openable
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -20,18 +24,34 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var drawerLayout: DrawerLayout
+
+    lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        drawerLayout = binding.drawerLayoutMain
+        bottomNavigationView = binding.bottomNavigationViewMain
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.memoFragment,
+                R.id.todoFragment,
+                R.id.groupFragment,
+                R.id.statsFragment
+            ),
+            drawerLayout
+        )
     }
 
     override fun onStart() {
         super.onStart()
-
         navController = findNavController(binding.navHostFragmentMain.id)
-        binding.bottomNavigationViewMain.setupWithNavController(navController)
+        bottomNavigationView.setupWithNavController(navController)
     }
 
 }
--- app/src/main/java/com/hansarang/android/air/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/hansarang/android/air/ui/fragment/HomeFragment.kt
@@ -5,16 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.hansarang.android.air.R
+import com.hansarang.android.air.databinding.FragmentHomeBinding
+import com.hansarang.android.air.ui.activity.MainActivity
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var toolbar: Toolbar
+    private val navController by lazy { findNavController() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        toolbar = binding.toolbarHome
+
+        if (requireActivity() is MainActivity) {
+            with(requireActivity() as MainActivity) {
+                setSupportActionBar(toolbar)
+                NavigationUI.setupWithNavController(
+                    toolbar,
+                    navController,
+                    appBarConfiguration
+                )
+                supportActionBar?.title = ""
+                supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+            }
+        }
     }
 
 }
