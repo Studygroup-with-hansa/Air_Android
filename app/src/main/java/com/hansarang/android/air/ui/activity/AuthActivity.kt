e0c625e52480bd6c4471eb8d75efa5071abab9e3
양윤재 <diddbswo04@dgsw.hs.kr>
2021-09-14 15:04:23 +0900

    [ADD] 로그인 프래그먼트 완성

---
+++ app/src/main/java/com/hansarang/android/air/ui/activity/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.hansarang.android.air.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hansarang.android.air.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
---
+++ app/src/main/java/com/hansarang/android/air/ui/fragment/SignInFragment.kt
@@ -0,0 +1,33 @@
+package com.hansarang.android.air.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.hansarang.android.air.R
+import com.hansarang.android.air.databinding.FragmentSignInBinding
+
+class SignInFragment : Fragment() {
+
+    private val navController by lazy { findNavController() }
+    private lateinit var binding: FragmentSignInBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSignInBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSignUpSignIn.setOnClickListener {
+            navController
+        }
+    }
+
+}
