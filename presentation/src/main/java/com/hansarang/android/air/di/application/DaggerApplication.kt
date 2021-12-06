0271e44f143fe44e0e225e9d0e9dd8f65ec43727
양윤재 <diddbswo04@dgsw.hs.kr>
2021-12-06 09:11:28 +0900

    [FIX] 백그라운드 서비스 수정

--- presentation/src/main/java/com/hansarang/android/air/di/application/DaggerApplication.kt
+++ presentation/src/main/java/com/hansarang/android/air/di/application/DaggerApplication.kt
@@ -1,10 +1,13 @@
 package com.hansarang.android.air.di.application
 
 import android.app.Application
+import android.app.NotificationManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import androidx.work.WorkManager
+import com.hansarang.android.air.background.worker.TimerWorker
 import com.hansarang.android.air.ui.util.SharedPreferenceHelper
 import com.hansarang.android.air.ui.util.SharedPreferenceHelper.init
 import dagger.hilt.android.HiltAndroidApp
@@ -31,14 +34,17 @@ class DaggerApplication(): Application(), Configuration.Provider {
             SharedPreferenceHelper.DARK_MODE -> AppCompatDelegate.setDefaultNightMode(
                 AppCompatDelegate.MODE_NIGHT_YES)
             SharedPreferenceHelper.SYSTEM_MODE -> {
-                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
-                }
+                AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
             }
         }
 
         super.onCreate()
+
+        val workManager = WorkManager.getInstance(applicationContext)
+        workManager.cancelAllWorkByTag(TimerWorker.TAG)
+
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.cancel(1)
     }
 }
