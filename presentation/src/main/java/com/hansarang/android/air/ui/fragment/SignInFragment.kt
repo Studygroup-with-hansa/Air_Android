c5f8778f4244698db30fbb08ce0a22036d255d5e
양윤재 <diddbswo04@dgsw.hs.kr>
2021-12-08 16:04:14 +0900

    [FIX] SignInFragment 리팩토링

--- presentation/src/main/java/com/hansarang/android/air/ui/fragment/SignInFragment.kt
+++ presentation/src/main/java/com/hansarang/android/air/ui/fragment/SignInFragment.kt
@@ -1,32 +1,30 @@
 package com.hansarang.android.air.ui.fragment
 
 import android.content.Intent
-import android.os.Bundle
 import android.os.CountDownTimer
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.util.Patterns
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.hansarang.android.air.R
 import com.hansarang.android.air.databinding.FragmentSignInBinding
 import com.hansarang.android.air.ui.activity.MainActivity
+import com.hansarang.android.air.ui.base.BaseFragment
 import com.hansarang.android.air.ui.livedata.EventObserver
 import com.hansarang.android.air.ui.util.SharedPreferenceHelper.token
 import com.hansarang.android.air.ui.viewmodel.fragment.SignInViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
-class SignInFragment : Fragment() {
+class SignInFragment : BaseFragment<FragmentSignInBinding, SignInViewModel>() {
 
-    private val navController by lazy { findNavController() }
-    private lateinit var binding: FragmentSignInBinding
-    private val viewModel: SignInViewModel by viewModels()
+    override val viewModel: SignInViewModel by viewModels()
+
     private val countDownTimer by lazy {
         object : CountDownTimer((5 * 60 * 1000).toLong(), 1000) {
             override fun onTick(millisUntilFinished: Long) {
@@ -48,57 +46,39 @@ class SignInFragment : Fragment() {
             }
         }
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentSignInBinding.inflate(inflater)
-        binding.vm = viewModel
-        binding.lifecycleOwner = viewLifecycleOwner
-
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        observe()
-    }
-
-    private fun observe() = with(viewModel) {
-        email.observe(viewLifecycleOwner) {
+    override fun observerViewModel() {
+        binding.etEmailSignIn.doAfterTextChanged {
             val emailChk = Patterns.EMAIL_ADDRESS
             binding.tilEmailSignIn.error =
-                if (!emailChk.matcher(it).matches()) {
+                if (!emailChk.matcher(it.toString()).matches()) {
                     resources.getString(R.string.please_set_validate_email)
                 } else {
                     ""
                 }
         }
 
-        authCode.observe(viewLifecycleOwner) {
-            signInButtonEnabled.value = it.isNotEmpty()
+        binding.etValidateCodeSignIn.doAfterTextChanged {
+            binding.btnSubmitSignIn.isEnabled = it.toString().isNotEmpty()
         }
 
-        isFailure.observe(viewLifecycleOwner, EventObserver {
+        viewModel.isFailure.observe(viewLifecycleOwner, EventObserver {
             Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
         })
 
-        isEmailSent.observe(viewLifecycleOwner) {
+        viewModel.isEmailSent.observe(viewLifecycleOwner) {
             if (it == true)
                 countDownTimer.start()
         }
 
-        isAuthSuccess.observe(viewLifecycleOwner) {
+        viewModel.isAuthSuccess.observe(viewLifecycleOwner) {
             token = "Token $it"
             Log.d("SignInViewModel", "postSendAuthCode: ${token}")
-            if (isEmailExist.value == true) {
+            if (viewModel.isEmailExist.value == true) {
                 val intent = Intent(requireContext(), MainActivity::class.java)
                 startActivity(intent)
                 requireActivity().finish()
             } else {
-                navController.navigate(R.id.action_signInFragment_to_signUpFragment)
+                findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
             }
         }
     }
