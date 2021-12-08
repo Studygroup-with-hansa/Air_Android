b5e8ef4e1ec7857c34f78b908db656b212c3c668
양윤재 <diddbswo04@dgsw.hs.kr>
2021-12-08 16:03:27 +0900

    [FIX] SettingFragment 리팩토링

--- presentation/src/main/java/com/hansarang/android/air/ui/fragment/SettingFragment.kt
+++ presentation/src/main/java/com/hansarang/android/air/ui/fragment/SettingFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDelegate.*
 import androidx.fragment.app.viewModels
 import com.hansarang.android.air.databinding.FragmentSettingBinding
+import com.hansarang.android.air.ui.base.BaseFragment
 import com.hansarang.android.air.ui.dialog.DialogAlertFragment
 import com.hansarang.android.air.ui.util.SharedPreferenceHelper.DARK_MODE
 import com.hansarang.android.air.ui.util.SharedPreferenceHelper.LIGHT_MODE
@@ -19,57 +20,19 @@ import com.hansarang.android.air.ui.viewmodel.fragment.SettingViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SettingFragment : Fragment() {
+class SettingFragment : BaseFragment<FragmentSettingBinding, SettingViewModel>() {
 
-    private val viewModel: SettingViewModel by viewModels()
-    private lateinit var binding: FragmentSettingBinding
+    override val viewModel: SettingViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentSettingBinding.inflate(inflater)
-        binding.vm = viewModel
-        binding.lifecycleOwner = viewLifecycleOwner
-        return binding.root
-    }
+    override fun observerViewModel() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        init()
-        observe()
-    }
-
-    private fun observe() = with(viewModel) {
-        lightMode.observe(viewLifecycleOwner) {
-            if (it) {
-                nightMode = LIGHT_MODE
-                setDefaultNightMode(MODE_NIGHT_NO)
-            }
+        with(viewModel) {
+            lightMode.value = (nightMode == LIGHT_MODE)
+            darkMode.value = (nightMode == DARK_MODE)
+            systemMode.value = (nightMode == SYSTEM_MODE)
         }
 
-        darkMode.observe(viewLifecycleOwner) {
-            if (it) {
-                nightMode = DARK_MODE
-                setDefaultNightMode(MODE_NIGHT_YES)
-            }
-        }
-
-        systemMode.observe(viewLifecycleOwner) {
-            if (it) {
-                nightMode = SYSTEM_MODE
-                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
-                    setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
-                } else {
-                    setDefaultNightMode(MODE_NIGHT_AUTO_BATTERY)
-                }
-            }
-        }
-    }
-
-    private fun init() = with(binding) {
-        btnLogoutSetting.setOnClickListener {
+        viewModel.logoutButtonClick.observe(viewLifecycleOwner) {
             val alert = DialogAlertFragment.newInstance("알림", "로그아웃 하시겠습니까?", "취소", "확인")
             alert.show(parentFragmentManager, "logoutAlert")
 
@@ -79,10 +42,29 @@ class SettingFragment : Fragment() {
             }
         }
 
-        with(viewModel) {
-            lightMode.value = (nightMode == LIGHT_MODE)
-            darkMode.value = (nightMode == DARK_MODE)
-            systemMode.value = (nightMode == SYSTEM_MODE)
+        viewModel.lightMode.observe(viewLifecycleOwner) {
+            if (it) {
+                nightMode = LIGHT_MODE
+                setDefaultNightMode(MODE_NIGHT_NO)
+            }
+        }
+
+        viewModel.darkMode.observe(viewLifecycleOwner) {
+            if (it) {
+                nightMode = DARK_MODE
+                setDefaultNightMode(MODE_NIGHT_YES)
+            }
+        }
+
+        viewModel.systemMode.observe(viewLifecycleOwner) {
+            if (it) {
+                nightMode = SYSTEM_MODE
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+                    setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
+                } else {
+                    setDefaultNightMode(MODE_NIGHT_AUTO_BATTERY)
+                }
+            }
         }
     }
 
